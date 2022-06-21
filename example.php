1ae5868a0654d06a9fa22d91616604bc56f7e0d3
Yasin <65618247+suphiyasin@users.noreply.github.com>
2022-06-21 10:14:59 +0300

    Create example.php

---
+++ example.php
@@ -0,0 +1,43 @@
+<?php
+include("api.php");
+$use = new dc();
+$use->cookie = '__dcfduid=412b5a70eb3f11eca5b82b8480bd3aa1; __sdcfduid=412b5a71eb3f11eca5b82b8480bd3aa18e55e20885a87da8c61926cad73162c26078e2fb8e954f423605595b36333e82; locale=tr; __cf_bm=fJn3kfUxAm_I5TMTDprz0ekGZYJVf0veTZsR_M8cg4k-1655655310-0-AcYk5JlSxrgWQyzvzsHdp+6liDrDWnUb3vFI1igGXwdFGhoSzxTMhps6kolEkxXpeNDaBxAVHLtD6aDXutQ8nKRm+W4C9dTHSZjBqEtBTRcfiX/s32JhGRCHG3aFjbEoMQ==';
+
+//I DONT USE DISCORD!
+
+//$use->login("mail", "password");
+
+//$use->auth = '';
+
+//$use->changeOwner("https://discord.com/channels/823979362306883645/823979362306883648", "https://discord.com/channels/823979362306883645/823979362306883648");
+
+//$use->createServer("saleam");
+
+//$use->deleteServer("https://discord.com/channels/988562948479910011/988562949226516504");
+
+//$use->sendServer("as", "https://discord.com/channels/557929250515320832/691231576394170378");
+
+//$use->PubStatu("Yo Bro Whatzup", "👍");
+
+//$use->getFriends();
+
+//$use->joinServer("reyhan");
+ 
+//$use->changeUsername("NEW USERNAME", Password");
+ 
+//DM link, username
+//$use->getUserInfo("https://discord.com/channels/@me/726369590874996777", "BuRNN");
+
+
+//$use->changePassword("Password", "NewPassword");
+
+//message, dm link, username
+//$use->sendMessage("Message", "https://discord.com/channels/@me/988176158640189490", "bedirhannkopar");
+
+
+//invisible, dnd, idle, online
+//$use->setStatu("online");
+
+//$use->profileinfo();
+
+//$use->messages();
